1f25b16bf2cec56c2f2fdc1299ee42f41e6f5eb4
NataliaZagovora <59080324+NataliaZagovora@users.noreply.github.com>
2019-12-26 20:46:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle R = new Rectangle(1, 3);
+            R.SquareCalculation(R.Side1, R.Side2);
+            Console.Write("Square rectangle: " + R.Square);
+            R.PerimeterCalculation(R.Side1, R.Side2);
+            Console.WriteLine();
+            Console.Write("Perimeter rectangle: " + R.Perimeter);
+            Console.WriteLine();
+            Console.WriteLine("**********************************");
+            Console.Write("Enter x=");
+            R.Side1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter y=");
+            R.Side2 = double.Parse(Console.ReadLine());
+            R.SquareCalculation(R.Side1, R.Side2);
+            Console.Write("Square rectangle: " + R.Square);
+            R.PerimeterCalculation(R.Side1, R.Side2);
+            Console.WriteLine();
+            Console.Write("Perimeter rectangle: " + R.Perimeter);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Rectangle
+    {
+        private double side1, side2;
+        private double square, perimeter;
+
+        public double Side1
+        {
+            get
+            { return side1; }
+            set
+            {
+                side1 = value;
+            }
+
+        }
+        public double Side2
+        {
+            get
+            { return side2; }
+            set
+            {
+                side2 = value;
+            }
+
+        }
+
+        public double Square
+        {
+            get
+            { return square; }
+        }
+        public double Perimeter
+        {
+            get
+            { return perimeter; }
+        }
+        public Rectangle()
+        { }
+        public Rectangle(double x, double y)
+        {
+            Side1 = x;
+            Side2 = y;
+        }
+        public void SquareCalculation(double x, double y)
+        {
+            if (x > 0 && y > 0)
+                square = x * y;
+            else
+                square = -1;
+        }
+        public void PerimeterCalculation(double x, double y)
+        {
+            if (x > 0 && y > 0)
+                perimeter = 2 * (x + y);
+            else
+                perimeter = -1;
+        }
+
+    }
+}
